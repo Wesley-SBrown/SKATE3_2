2a0881f9f2d80ca720406b247cc276f4d375d370
Wesley Brown <wsb.wesleybrown@gmail.com>
2026-08-11 16:47:06 -0700

    docs: stats_recorder docs

--- src/seismogram_pipeline/core/stats_recorder.py
+++ src/seismogram_pipeline/core/stats_recorder.py
@@ -1,20 +1,44 @@
 import json
-
+from typing import Any
 
 class Record:
+    """
+    A utility class for recording and exporting application statistics and metrics to JSON files
+    """
     active = False
     stats = {}
 
     @classmethod
-    def activate(cls):
+    def activate(cls) -> None:
+        """
+        Activate the recording utility
+        """
         cls.active = True
 
     @classmethod
-    def record(cls, key, value):
+    def record(cls, key: str, value: Any) -> None:
+        """
+        Record a statistic under a specified key
+
+        Parameters
+        ----------
+        key : str
+            The identifier/name of the statistic to record
+        value : Any
+            The value associated with the statistic
+        """
         cls.stats[key] = value
 
     @classmethod
-    def export_as_json(cls, filename):
+    def export_as_json(cls, filename: str) -> None:
+        """
+        Export current recorded stats, appending them to an existing JSON record file or creating a new one
+
+        Parameters
+        ----------
+        filename : str
+            The filename of the JSON file where stats should be exported
+        """
         try:
             with open(filename, "r+") as myfile:
                 data = myfile.read()
